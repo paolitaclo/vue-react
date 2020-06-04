dcf0d63a37cee14011dc02bbcb8649c36a25f7b8
paolitaclo <paolita.claros@gmail.com>
2020-06-04 07:09:42 -0700

    Update vue-calculator/src/components

--- vue-calculator/src/App.vue
+++ vue-calculator/src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DigitContainer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DigitContainer from './components/DigitContainer/index'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DigitContainer
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ vue-calculator/src/components/DigitBox/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="digit-box" v-bind:class="styledBox" @click="onSelectDigit(digitNumber)">{{digitNumber}}</div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        props: {
+            digitNumber: {
+                type: String,
+                required: true
+            },
+            styledBox: {
+                type: String,
+                required: false
+            }
+        },
+        methods: {
+            onSelectDigit(value) {
+                this.$emit('onSelectDigit', value);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .digit-box {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100px;
+        height: 100px;
+        font-size: 50px;
+        border: 2px solid red;
+        background-color: coral;
+    }
+
+    .operator {
+        background-color: aliceblue;
+    }
+</style>
